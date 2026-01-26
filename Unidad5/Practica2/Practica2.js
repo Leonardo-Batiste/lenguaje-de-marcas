004c7c83a660ff4c88a964a251e20ad5c8ec51d6
Leonardo Batiste <leonardo.batiste.estudios@gmail.com>
2026-01-26 10:15:39 +0100

    Empezado Tema5Practica2 y terminado la actividad 1 de este

---
+++ Unidad5/Practica2/Practica2.js
@@ -0,0 +1,18 @@
+/* Actividad1:
+mas = document.getElementById("mas");
+function mostrarMasTexto(){
+	mas.innerHTML = "Se utiliza principalmente del lado del cliente, implementado como parte" 
+	+ "de un navegador web permitiendo mejoras en la interfaz de usuario y páginas web dinámicas[3] y "
+	+ "JavaScript del lado del servidor (Server-side JavaScript o SSJS). Su uso en aplicaciones externas a" 
+	+ "la web, por ejemplo en documentos PDF, aplicaciones de escritorio (mayoritariamente widgets) es" 
+	+ "también significativo.";
+}
+mas.addEventListener("click", mostrarMasTexto);
+*/
+
+//Actividad2
+tabla=document.getElementById('table');
+function pedirDatos(){
+	let dato1 = prompt("Introduce tu año de nacimiento.");
+	let dato2 = prompt("introduce tu nombre,");
+}
